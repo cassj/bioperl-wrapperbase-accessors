a025f225dc79f117e221c39036b4fc990c7e16c6
cassj <cassjohnston@gmail.com>
2011-01-09 19:26:34 +0000

    Separated parameters and switches. Added parameter_string method.

--- Bio-Tools-Run-WrapperBase-Accessor/lib/Bio/Tools/Run/WrapperBase/Accessor.pm
+++ Bio-Tools-Run-WrapperBase-Accessor/lib/Bio/Tools/Run/WrapperBase/Accessor.pm
@@ -50,33 +50,51 @@ use base 'Bio::Tools::Run::WrapperBase';
 # $self->is_valid_parameter($param_name)
 
 
-=head2 _register_parameters
+=head2 _setup
 
-Title    : _register_parameters
+Title    : _setup
 Usage    : package MyWrapperClass;
            use base 'Bio::Tools::Run::WrapperBase::Accessor';
            my $Parameters = {foo => 'A description of foo', bar => 'A description of bar'};
-           __PACKAGE__->_register_parameters($Parameters);
-Function : Creates class methods valid_parameters and is_valid_parameter($name)
-           and accessors for each of your parameter names in which to store argument values.
-Returns  : 1 to indicate success.
+           my $Switches   = {stuff => 'A description of stuff', thing => 'A description of thing'}
+           __PACKAGE__->_setup(-params => $Parameters, -switches => $Switches);
+Function : Creates class methods ->valid_parameters, valid_switches which return an array of valid 
+           parameters or switches
+           Creates class methods ->is_valid_parameter($name) and ->is_valid_switch($name) 
+           which will return true if the given $name is a valid parameter or switch
+           Creates accessors for each of your parameter and switch names in which to store values.
+           Creates methods ->parameters and ->switches which return copies of the initial parameter or switch
+           hash.
+Returns  : Nothing.
 Args     : None
 
 
 =cut
-sub _register_parameters{
+sub _setup{
     my $class = shift;
-    &class::throw("_mk_param_accessors is a class method. If you're calling it from an object you're doing it wrong.") if ref($class);
-    my $params = shift || {};
-    
+    &class::throw("_setup is a class method. If you're calling it from an object you're doing it wrong.") if ref($class);
+ 
+    my ($params, $swtchs) = ({},{});
+    ($params, $swtchs) = $class->_rearrange([qw/params switches/], @_);
+
     my @valid_params = sort {$a cmp $b} keys %{$params};
     my $valid_parameters = sub { return @valid_params; };
+
+    my @valid_switches = sort {$a cmp $b} keys %{$swtchs};
+    my $valid_switches = sub { return @valid_switches; };
 
     my $is_valid_parameter = sub {
       my ($self, $p) = @_;
       $p = lc($p); # Because _rearrange lets you specify params in uc, lc or a mix
       my %ps = %$params;
       return exists $ps{$p};
+    };
+
+    my $is_valid_switch = sub {
+      my ($self, $s) = @_;
+      $s = lc($s); 
+      my %ss = %$swtchs;
+      return exists $ss{$s};
     };
 
     # Return parameter hash, by value
@@ -88,16 +106,28 @@ sub _register_parameters{
       return \%data;
     };
 
+    # Return switch hash, by value
+    my $switches  = sub {
+      my ($self,@which) = @_;
+      @which = keys %$swtchs unless scalar(@which);
+      my %data;
+      @data{@which} = @$swtchs{@which};
+      return \%data;
+    };
+
     # add the methods to the class
     {
       no strict 'refs';
-      *{"$class\::valid_parameters"}  = $valid_parameters;
-      *{"$class\::is_valid_parameter"} = $is_valid_parameter;
-      *{"$class\::parameters"} = $parameters;
+      *{"$class\::valid_parameters"}    = $valid_parameters;
+      *{"$class\::valid_switches"}      = $valid_switches;
+      *{"$class\::is_valid_parameter"}  = $is_valid_parameter;
+      *{"$class\::is_valid_switch"}     = $is_valid_switch;
+      *{"$class\::parameters"}          = $parameters;
+      *{"$class\::switches"}            = $switches;
     }
 
     # and generate accessors for each of the values
-    foreach my $param ($class->valid_parameters){
+    foreach my $param ($class->valid_parameters, $class->valid_switches){
       my $accessor = sub{
 	my $self = shift;
 	if (scalar(@_) > 0){
@@ -121,8 +151,8 @@ sub new {
   my $self = $class->SUPER::new(@_); 
   bless $self, $class;
 
-  #don't know why. It's a bioperl thing. 
-  my @params = map {uc($_)} $self->valid_parameters;
+  # Bioperl standards say to use _rearrange
+  my @params = map {uc($_)} ($self->valid_parameters, $self->valid_switches);
   my @values = $self->_rearrange(\@params, @_);
   
   foreach (@params){
@@ -131,6 +161,33 @@ sub new {
     $self->$param($val) if $val;
   }
   return $self;
+}
+
+
+
+=head2 parameter_string
+
+  Title     : parameter_string
+  Usage     : my $ps = $folder->parameter_string(-double_dash => 1,
+                                                 -underscore_to_dash => 1);
+  Function  : Returns a parameter string generated from current parameter/switch settings.
+              Passes any given arguments to WrapperBase's _setparams method
+  Returns   : String
+  Args      : None
+
+=cut
+
+sub parameter_string {
+  my $self = shift;
+  my @defined_params = grep {defined $self->$_} $self->valid_parameters;
+  my @defined_switches = grep {defined $self->$_} $self->valid_switches;
+
+  # use the wrapperbase _setparams helper.
+  my $string = $self->_setparams(-params => \@defined_params,
+				 -switches => \@defined_switches,
+				 @_);
+
+  return $string;
 }
 
 
--- Bio-Tools-Run-WrapperBase-Accessor/t/01.t
+++ Bio-Tools-Run-WrapperBase-Accessor/t/01.t
@@ -6,7 +6,7 @@ use Bio::Root::Test;
 
 use Bio::Tools::Run::WrapperBase::Accessor;
 
-test_begin(-tests => 25);
+test_begin(-tests => 42);
 
 # Make a subclass to test:
 push @ATestClass::ISA, 'Bio::Tools::Run::WrapperBase::Accessor';
@@ -15,16 +15,24 @@ push @ATestClass::ISA, 'Bio::Tools::Run::WrapperBase::Accessor';
 $ATestClass::Parameters = {
 			    foo => 'a numeric value',
 			    bar => 'a string',
-			    baz => 'a boolean value'
+			    baz => 'a description'
 			   };
 
-can_ok('ATestClass', '_register_parameters');
-ok(ATestClass->_register_parameters($ATestClass::Parameters), 'registering parameters');
+$ATestClass::Switches = {
+			 stuff => 'a description of stuff',
+			 thing => 'a description of thing'
+			};
+
+can_ok('ATestClass', '_setup');
+ok(ATestClass->_setup('-params' => $ATestClass::Parameters, '-switches' => $ATestClass::Switches), 'registering parameters');
 
 # check we've added the parameter accessors
 can_ok('ATestClass', 'valid_parameters');
+can_ok('ATestClass', 'valid_switches');
 can_ok('ATestClass', 'is_valid_parameter');
+can_ok('ATestClass', 'is_valid_switch');
 can_ok('ATestClass', 'parameters');
+can_ok('ATestClass', 'switches');
 
 
 # and argument accessors for each parameter
@@ -32,6 +40,9 @@ can_ok('ATestClass', 'foo');
 can_ok('ATestClass', 'bar');
 can_ok('ATestClass', 'baz');
 
+# and each switch
+can_ok('ATestClass', 'stuff');
+can_ok('ATestClass', 'thing');
 
 # and that we can create a new object
 my $obj = ATestClass->new();
@@ -42,29 +53,46 @@ ok($obj->is_valid_parameter("foo"));
 ok($obj->is_valid_parameter("Foo")); # _rearrange lets you use lc or uc or mix of both.
 ok($obj->is_valid_parameter("bAr")); # which I think is daft, but meh.
 ok(!$obj->is_valid_parameter("cheesecake"));
+ok($obj->is_valid_switch('stuff'));
+ok(!$obj->is_valid_switch('monkey'));
+
 
 #returns params in alphabetical order.
 is_deeply([$obj->valid_parameters],[qw(bar baz foo)]);
+
+#returns switches in alphabetical order
+is_deeply([$obj->valid_switches], [qw(stuff thing)]);
 
 # returns the parameter hash for the given params
 is_deeply($obj->parameters, $ATestClass::Parameters);
 is_deeply($obj->parameters('foo'), {foo => 'a numeric value'});
 is_deeply($obj->parameters('foo','bar'), {foo=>'a numeric value', bar=>'a string'});
 
-#and just check it really does pass by value, not reference
-my $param_copy = $obj->parameters;
+# returns the switch hash for the given switches
+is_deeply($obj->switches, $ATestClass::Switches);
+is_deeply($obj->switches('stuff'), {stuff => 'a description of stuff'});
+is_deeply($obj->switches('stuff','thing'), {stuff=>'a description of stuff', thing=>'a description of thing'});
+
+
+# and just check it really does pass by value, not reference
+# as we don't want people accidentally dicking with the class data
+my $param_copy = ATestClass->parameters;
 $param_copy->{test} = "blah blah blah";
-isnt($ATestClass::Parameters->{test}, $param_copy->{test});
+isnt(ATestClass->parameters->{test}, $param_copy->{test});
+
+my $switch_copy = ATestClass->switches;
+$switch_copy->{test} = "blah blah blah";
+isnt(ATestClass->switches->{test}, $param_copy->{test});
 
 # and use those accessors
 is($obj->foo(123), 123);
-is($obj->foo(), 123);
-is($obj->foo(0), 0);
+is($obj->foo, 123);
+ok(!defined $obj->foo(undef));
 
-# And we're using undef to mean don't pass this parameter,
-# so check we can undef stuff.
-$obj->foo(undef);
-ok(! defined $obj->foo());
+is($obj->stuff(1), 1);
+is($obj->stuff, 1);
+ok(!defined $obj->stuff(undef));
+
 
 # Can we set values in the constructor. I don't really understand
 # why Bioperl insists on using _rearrange and allowing params
@@ -78,4 +106,12 @@ is($obj->foo, 123);
 
 $obj = ATestClass->new('-FoO' => 123);
 is($obj->foo, 123);
+
+$obj = ATestClass->new('-Foo' => 123, '-stuFF' => 1);
+is($obj->stuff, 1);
+
+
+# And can we create an appropriate parameter string
+can_ok($obj, 'parameter_string');
+is($obj->parameter_string(-double_dash=>1), ' --foo 123 --stuff');
 
